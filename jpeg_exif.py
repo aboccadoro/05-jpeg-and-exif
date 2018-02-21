e93f3cc93d204ea3a940d7848896013edd96e99d
Anthony Boccadoro <aboccadoro@umass.edu>
2018-02-21 00:31:19 -0500

    find_jfif(): #found S/EOI tags + indexes

--- jpeg_exif.py
+++ jpeg_exif.py
@@ -27,14 +27,41 @@ def carve(f, start, end):
 
 
 def find_jfif(f, max_length=None):
-    # do some stuff
-
-    # then return a possibly-empty sequence of pairs
-
-    # here's an example that just returns the start and end of the file without parsing
-    chunk = f.read()
-    last_byte = len(chunk)
-    return [(0, last_byte)]
+    offsets = []
+    offset = 0
+    chunk = b''
+    while True:
+        if max_length != None:
+            if offset + 256 > max_length: 
+                if max_length % 256 == 0:
+                    chunk = f.read(256)
+                else:
+                    chunk = f.read(max_length % 256)
+            else:
+                chunk = f.read(256)
+        else:
+            chunk = f.read(256)
+        if chunk == b'': break
+        end = ((len(chunk) - 1) % 256) + 1
+        for i in range(0, end):
+            first_byte = hex(chunk[i])
+            if first_byte == '0xff' and i + 1 < end:
+                next_byte = hex(chunk[i + 1])
+                if next_byte == '0xd8':
+                    offsets += [(offset, next_byte)]
+                elif next_byte == '0xd9':
+                    offsets += [(offset + 1, next_byte)]
+            elif first_byte == '0xff' and i + 1 >= end:
+                seek = f.read(1)
+                if seek != b'':
+                    next_byte = hex(seek[0])
+                    if next_byte == '0xd8':
+                        offsets += [(offset, next_byte)]
+                    elif next_byte == '0xd9':
+                        offsets += [(offset + 1, next_byte)]
+            offset += 1
+    for item in offsets:
+        print(item)
 
 
 def parse_exif(f):
@@ -44,3 +71,9 @@ def parse_exif(f):
 
     # Don't hardcode the answer! Return your computed dictionary.
     return {'Make':['Apple']}
+
+def main():
+    find_jfif(open("gore-superman.jpg", 'rb'))
+
+if __name__ == '__main__':
+    main()
